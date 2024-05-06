d2548a310e0e502b48b03afc265716eca4dd0412
Aleksandar <Sasho80@gbg.bg>
2024-05-06 20:47:23 +0300

    Add files via upload

---
+++ 01.day_of_week.py
@@ -0,0 +1,20 @@
+num_for_day = int(input())
+
+if num_for_day == 1:
+    print("Monday")
+elif num_for_day == 2:
+    print("Tuesday")
+elif num_for_day == 3:
+    print("Wednesday")
+elif num_for_day == 4:
+    print("Thursday")
+elif num_for_day == 5:
+    print("Friday")
+elif num_for_day == 6:
+    print("Saturday")
+elif num_for_day == 7:
+    print("Sunday")
+else:
+    print("Error")
+
+
